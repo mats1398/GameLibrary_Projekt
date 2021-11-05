94b6c67aa3ca7385205fa5f81b46ad18404187a1
MatsRamsl <matsramsl@online.de>
2021-11-05 09:31:01 +0100

    Erster Git-Test

---
+++ GameLibrary_Projekt/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary_Projekt
+{
+    public class Game
+    {
+
+        // Eigenschaften
+
+    }
+}
