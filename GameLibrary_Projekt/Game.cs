d550723441b3e4b866cb960e14ad018632afe34c
MatsRamsl <matsramsl@online.de>
2021-11-09 11:32:50 +0100

    Erstes Grundgerüst, für die Eigenschaften der Spiele

--- GameLibrary_Projekt/Game.cs
+++ GameLibrary_Projekt/Game.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace GameLibrary_Projekt
 {
-    public class Game
+    public class Game 
     {
-
         // Eigenschaften
+        public string Titel { get; set; }
+        public DateTime Erscheinungsdatum { get; set; }
+
+        public string[] Plattform { get; set; }
+        public decimal Rating { get; set; }
+        public string Genre { get; set; }
+
+       
 
     }
 }
